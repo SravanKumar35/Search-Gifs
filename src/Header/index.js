029c32800e91fcee54c828bcd3588eb7f7f84817
SravanKumar35 <kumarsravan9182@gmail.com>
2022-07-06 20:03:38 +0530

    added icon in header

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,9 +1,13 @@
 import React from "react";
-import styles from './styles.module.scss'
+import HideImageIcon from "@mui/icons-material/HideImage";
+import styles from "./styles.module.scss";
 
 const Header = () => {
   return (
     <div className={styles.header}>
+      <div>
+        <HideImageIcon sx={{ fontSize: 80 }} />
+      </div>
       <span>Hello, Giphy</span>
     </div>
   );
